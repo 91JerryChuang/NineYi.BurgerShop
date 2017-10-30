abe3af2748a88d390526133a49d2ad79b4f6e77b
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-10-30 18:00:21 +0800

    新建 ICookable 定義可以烹飪方法的介面。

---
+++ NineYi.BurgerShop/Services/Interfaces/ICookable.cs
@@ -0,0 +1,16 @@
+﻿namespace NineYi.BurgerShop.Services.Interfaces
+{
+    /// <summary>
+    /// 定義可以烹飪的方法。
+    /// </summary>
+    public interface ICookable
+    {
+        /// <summary>
+        /// 烹飪指定的食物。
+        /// </summary>
+        /// <typeparam name="TFood">食物的型別。</typeparam>
+        /// <param name="food">食物。</param>
+        /// <returns>烹飪後的食物。</returns>
+        string Cook<TFood>(TFood food);
+    }
+}
