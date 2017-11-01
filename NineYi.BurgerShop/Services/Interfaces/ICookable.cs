0c03e45d199d1cb7b2d92ee10f483a38de28337a
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-01 17:53:37 +0800

    調整 ICookable.Cook 方的定義不使用泛型方法。

--- NineYi.BurgerShop/Services/Interfaces/ICookable.cs
+++ NineYi.BurgerShop/Services/Interfaces/ICookable.cs
@@ -1,4 +1,6 @@
-﻿namespace NineYi.BurgerShop.Services.Interfaces
+﻿using NineYi.BurgerShop.Models.Interfaces;
+
+namespace NineYi.BurgerShop.Services.Interfaces
 {
     /// <summary>
     /// 定義可以烹飪的方法。
@@ -8,9 +10,8 @@
         /// <summary>
         /// 烹飪指定的菜餚。
         /// </summary>
-        /// <typeparam name="TDish">菜餚的型別。</typeparam>
         /// <param name="food">菜餚。</param>
         /// <returns>烹飪後的菜餚。</returns>
-        string Cook<TDish>(TDish dish);
+        string Cook(IDish dish);
     }
 }
