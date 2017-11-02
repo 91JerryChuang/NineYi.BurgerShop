0b55574d34fb099b42f829cf048ea04ad63a14bd
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 14:25:56 +0800

    在 BranchStoreEnum 的欄位上增加 DescriptionAttribute。

--- NineYi.BurgerShop/Commons/Enums/BranchStoreEnum.cs
+++ NineYi.BurgerShop/Commons/Enums/BranchStoreEnum.cs
@@ -1,4 +1,6 @@
-﻿namespace NineYi.BurgerShop.Commons.Enums
+﻿using System.ComponentModel;
+
+namespace NineYi.BurgerShop.Commons.Enums
 {
     /// <summary>
     /// 分店列舉。
@@ -8,16 +10,19 @@
         /// <summary>
         /// 台北。
         /// </summary>
+        [Description("台北")]
         Taipei = 0,
 
         /// <summary>
         /// 紐約。
         /// </summary>
+        [Description("紐約")]
         NewYork = 1,
 
         /// <summary>
         /// 東京。
         /// </summary>
+        [Description("東京")]
         Tokyo = 2
     }
 }
