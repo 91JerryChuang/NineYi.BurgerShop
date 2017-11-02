1cd0f2481c4c7e24e9e9e8f0520b54c29ecfa774
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 15:02:16 +0800

    移除未使用的 Meat.ToString 方法。

--- NineYi.BurgerShop/Models/Meats/Meat.cs
+++ NineYi.BurgerShop/Models/Meats/Meat.cs
@@ -12,16 +12,5 @@
         /// 名稱。
         /// </value>
         public string Name { get; protected set; }
-
-        /// <summary>
-        /// 傳回這個 <see cref="System.String" /> 的執行個體；不會執行實際的轉換。
-        /// </summary>
-        /// <returns>
-        /// 目前的字串。
-        /// </returns>
-        public override string ToString()
-        {
-            return this.Name;
-        }
     }
 }
