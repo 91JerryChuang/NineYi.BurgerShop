e63247e5b9b16737e9b47d8839016773fa3eb809
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 12:10:46 +0800

    1. 新建 BurgerEnum 漢堡列舉。 2. 新建 ChefFactory 廚師工廠類別。

---
+++ NineYi.BurgerShop/Commons/Enums/BurgerEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace NineYi.BurgerShop.Commons.Enums
+{
+    /// <summary>
+    /// 漢堡列舉。
+    /// </summary>
+    public enum BurgerEnum
+    {
+        /// <summary>
+        /// 香雞堡。
+        /// </summary>
+        Chicken = 0,
+
+        /// <summary>
+        /// 豬肉堡。
+        /// </summary>
+        Pork = 1
+    }
+}
