c9e57d6768792ffb2a611a1d81a17d0f53764937
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 15:44:41 +0800

    新建 ThousandIsland 千島醬的類別。

---
+++ NineYi.BurgerShop/Models/Sauces/ThousandIsland.cs
@@ -0,0 +1,17 @@
+﻿namespace NineYi.BurgerShop.Models.Sauces
+{
+    /// <summary>
+    /// 千島醬。
+    /// </summary>
+    /// <seealso cref="NineYi.BurgerShop.Models.Sauces.Sauce" />
+    public class ThousandIsland : Sauce
+    {
+        /// <summary>
+        /// 初始化 <see cref="ThousandIsland"/> 類別新的執行個體。
+        /// </summary>
+        public ThousandIsland()
+        {
+            this.Name = "Thousand Island";
+        }
+    }
+}
