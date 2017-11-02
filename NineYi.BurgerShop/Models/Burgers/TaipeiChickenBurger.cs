61527bb6f28fe48770a7cf1a02fd112bf84ba86c
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 15:03:39 +0800

    調整 TaipeiChickenBurger 建構式的 XML 註解。

--- NineYi.BurgerShop/Models/Burgers/TaipeiChickenBurger.cs
+++ NineYi.BurgerShop/Models/Burgers/TaipeiChickenBurger.cs
@@ -11,8 +11,11 @@ namespace NineYi.BurgerShop.Models.Burgers
     public class TaipeiChickenBurger : Burger
     {
         /// <summary>
-        /// 初始化 <see cref="TaipeiChickenBurger"/> 類別新的執行個體。
+        /// 初始化 <see cref="TaipeiChickenBurger" /> 類別新的執行個體。
         /// </summary>
+        /// <param name="bread">麵包。</param>
+        /// <param name="veggie">蔬菜。</param>
+        /// <param name="meat">肉類。</param>
         public TaipeiChickenBurger(Bread bread, Veggie veggie, Meat meat)
             : base("Taipei Chicken Burger", bread, veggie, meat)
         {
