9593b94e1d334e18bc8f16ba1d376966c634bcc3
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 14:26:11 +0800

    在 BranchStoreEnum 的欄位上增加 DescriptionAttribute。

--- NineYi.BurgerShop/Commons/Enums/BurgerEnum.cs
+++ NineYi.BurgerShop/Commons/Enums/BurgerEnum.cs
@@ -1,4 +1,6 @@
-﻿namespace NineYi.BurgerShop.Commons.Enums
+﻿using System.ComponentModel;
+
+namespace NineYi.BurgerShop.Commons.Enums
 {
     /// <summary>
     /// 漢堡列舉。
@@ -8,11 +10,13 @@
         /// <summary>
         /// 香雞堡。
         /// </summary>
+        [Description("香雞堡")]
         Chicken = 0,
 
         /// <summary>
         /// 豬肉堡。
         /// </summary>
+        [Description("豬肉堡")]
         Pork = 1
     }
 }
