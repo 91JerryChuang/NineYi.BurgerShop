89e01c5f4588e1886a47beeb9dd4034c70d77cdc
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 12:07:41 +0800

    新增 NewYorkChickenBurger 建構式的傳入參數 bread、veggie、meat 並設定初始化。

--- NineYi.BurgerShop/Models/Burgers/NewYorkChickenBurger.cs
+++ NineYi.BurgerShop/Models/Burgers/NewYorkChickenBurger.cs
@@ -1,4 +1,8 @@
-﻿namespace NineYi.BurgerShop.Models.Burgers
+﻿using NineYi.BurgerShop.Models.Breads;
+using NineYi.BurgerShop.Models.Meats;
+using NineYi.BurgerShop.Models.Veggies;
+
+namespace NineYi.BurgerShop.Models.Burgers
 {
     /// <summary>
     /// 紐約香雞堡。
@@ -9,9 +13,9 @@
         /// <summary>
         /// 初始化 <see cref="NewYorkChickenBurger"/> 類別新的執行個體。
         /// </summary>
-        public NewYorkChickenBurger()
+        public NewYorkChickenBurger(Bread bread, Veggie veggie, Meat meat)
+            : base("NewYork Chicken Burger", bread, veggie, meat)
         {
-            this.Name = "NewYork Chicken Burger";
         }
     }
 }
