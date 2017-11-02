b122280fd077b479a4b322d156e8d521273a843b
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 15:13:03 +0800

    新建 GrainBread 穀類麵包的類別。

---
+++ NineYi.BurgerShop/Models/Breads/GrainBread.cs
@@ -0,0 +1,17 @@
+﻿namespace NineYi.BurgerShop.Models.Breads
+{
+    /// <summary>
+    /// 穀類麵包。
+    /// </summary>
+    /// <seealso cref="NineYi.BurgerShop.Models.Breads.Bread" />
+    public class GrainBread : Bread
+    {
+        /// <summary>
+        /// 初始化 <see cref="GrainBread"/> 類別新的執行個體。
+        /// </summary>
+        public GrainBread()
+        {
+            this.Name = "Grain Bread";
+        }
+    }
+}
