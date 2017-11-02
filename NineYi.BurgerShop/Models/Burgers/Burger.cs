2c5d7e41fa09b3d2bf2d11f4c53eed91d23dcbb4
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 12:06:09 +0800

    1. 調整 Burger.Name 名稱屬性的存取與設定方法。 2. 調整 Burger.Bread 麵包屬性的存取與設定方法。 3. 調整 Burger.Veggie 蔬菜屬性的存取與設定方法。 4. 調整 Burger.Meat 肉類屬性的存取與設定方法。 5. 新建 Burger 的建構式並設定初始化。

--- NineYi.BurgerShop/Models/Burgers/Burger.cs
+++ NineYi.BurgerShop/Models/Burgers/Burger.cs
@@ -1,4 +1,7 @@
-﻿using NineYi.BurgerShop.Models.Breads;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NineYi.BurgerShop.Models.Breads;
 using NineYi.BurgerShop.Models.Interfaces;
 using NineYi.BurgerShop.Models.Meats;
 using NineYi.BurgerShop.Models.Veggies;
@@ -12,36 +15,78 @@ namespace NineYi.BurgerShop.Models.Burgers
     public abstract class Burger : IDish
     {
         /// <summary>
-        /// 取得或設定名稱。
+        /// 名稱。
+        /// </summary>
+        private readonly string _name;
+
+        /// <summary>
+        /// 取得名稱。
         /// </summary>
         /// <value>
         /// 名稱。
         /// </value>
-        public string Name { get; protected set; }
+        public string Name { get { return _name; } }
 
         /// <summary>
-        /// 取得或設定麵包。
+        /// 麵包。
+        /// </summary>
+        private readonly Bread _bread;
+
+        /// <summary>
+        /// 取得麵包。
         /// </summary>
         /// <value>
         /// 麵包。
         /// </value>
-        public Bread Bread { get; set; }
+        public Bread Bread { get { return _bread; } }
 
         /// <summary>
-        /// 取得或設定蔬菜。
+        /// 蔬菜。
+        /// </summary>
+        private readonly Veggie _veggie;
+
+        /// <summary>
+        /// 取得蔬菜。
         /// </summary>
         /// <value>
         /// 蔬菜。
         /// </value>
-        public Veggie Veggie { get; set; }
+        public Veggie Veggie { get { return _veggie; } }
 
         /// <summary>
-        /// 取得或設定肉類。
+        /// 肉類。
+        /// </summary>
+        private readonly Meat _meat;
+
+        /// <summary>
+        /// 取得肉類。
         /// </summary>
         /// <value>
         /// 肉類。
         /// </value>
-        public Meat Meat { get; set; }
+        public Meat Meat { get { return _meat; } }
+
+        public Burger(string name, Bread bread, Veggie veggie, Meat meat)
+        {
+            var paramsChecklist = new Dictionary<string, Func<bool>>
+            {
+                { nameof(name), () => string.IsNullOrWhiteSpace(name) },
+                { nameof(bread), () => bread == null },
+                { nameof(veggie), () => veggie == null },
+                { nameof(meat), () => meat == null }
+            };
+
+            var paramName = paramsChecklist.FirstOrDefault(x => x.Value() == true).Key;
+            if (string.IsNullOrWhiteSpace(paramName) == false)
+            {
+                throw new ArgumentException(paramName);
+            }
+
+            this._name = name;
+            this._bread = bread;
+            this._veggie = veggie;
+            this._meat = meat;
+        }
 
         /// <summary>
         /// 取得烹飪的方法。
