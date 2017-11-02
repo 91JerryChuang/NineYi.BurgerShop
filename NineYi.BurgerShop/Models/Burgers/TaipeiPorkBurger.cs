a8fcbe4ef02f191d6d2c91de1d9ab7effc61e839
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-02 12:08:21 +0800

    新增 TaipeiPorkBurger 建構式的傳入參數 bread、veggie、meat 並設定初始化。

--- NineYi.BurgerShop/Models/Burgers/TaipeiPorkBurger.cs
+++ NineYi.BurgerShop/Models/Burgers/TaipeiPorkBurger.cs
@@ -1,4 +1,8 @@
-﻿namespace NineYi.BurgerShop.Models.Burgers
+﻿using NineYi.BurgerShop.Models.Breads;
+using NineYi.BurgerShop.Models.Meats;
+using NineYi.BurgerShop.Models.Veggies;
+
+namespace NineYi.BurgerShop.Models.Burgers
 {
     /// <summary>
     /// 台北豬肉堡。
@@ -9,9 +13,9 @@
         /// <summary>
         /// 初始化 <see cref="TaipeiPorkBurger"/> 類別新的執行個體。
         /// </summary>
-        public TaipeiPorkBurger()
+        public TaipeiPorkBurger(Bread bread, Veggie veggie, Meat meat)
+            : base("Taipei Pork Burger", bread, veggie, meat)
         {
-            this.Name = "Taipei Pork Burger";
         }
     }
 }
